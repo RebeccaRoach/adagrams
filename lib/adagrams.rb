d2366318b6574b6126eba862d6a577e854ecee48
RebeccaRoach <rebeccaroach12@gmail.com>
2020-02-14 14:47:37 -0800

    Cleaned all waves, ready to submit

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,7 +1,8 @@
+# Defining requirements and constants
 require "csv"
 require "awesome_print"
 
-$LETTERS = {
+LETTERS = {
   "A" => 9,
   "B" => 2,
   "C" => 2,
@@ -30,38 +31,7 @@ $LETTERS = {
   "Z" => 1
 }
 
-# Wave 1
-def draw_letters
-  pool = []
-  $LETTERS.map do |letter, tiles|
-    tiles.times do 
-      pool << letter
-    end
-  end
-  pool = pool.shuffle.drop(88)
-  return pool
-end
-
-# Wave 2
-def uses_available_letters?(input, letters_in_hand)
-  input_array = Array.new(input.chars)
-  current_hand = Array.new(letters_in_hand)
-  input_array.map do |letter|
-    puts "The letter is #{letter}"
-    if current_hand.index(letter) != nil
-      puts "Found the letter!"
-      puts "Here is the index: #{current_hand.index(letter)}"
-      current_hand.delete_at(current_hand.index(letter))
-      puts "Here is the array now: #{current_hand}"
-    else
-      return false
-    end
-  end
-  return true
-end
-
-# Wave 3
-$LETTER_VALUES = {
+POINT_VALUES = {
   "A" => 1,	
   "B" => 3,	
   "C" => 3,	
@@ -90,11 +60,40 @@ $LETTER_VALUES = {
   "Z" => 10
 }
 
+DICTIONARY = "../assets/dictionary-english.csv"
+
+# Wave 1
+def draw_letters
+  pool = []
+  LETTERS.map do |letter, num_of_tiles|
+    num_of_tiles.times do 
+      pool << letter
+    end
+  end
+  pool = pool.shuffle.drop(88)
+  return pool
+end
+
+# Wave 2
+def uses_available_letters?(input, letters_in_hand)
+  input_array = Array.new(input.chars)
+  current_hand = Array.new(letters_in_hand)
+  input_array.map do |letter|
+    if current_hand.index(letter) != nil
+      current_hand.delete_at(current_hand.index(letter))
+    else
+      return false
+    end
+  end
+  return true
+end
+
+# Wave 3
 def score_word(word)
   total_points = 0
   word_array = Array.new(word.upcase.chars)
   points = word_array.map do |char|
-    $LETTER_VALUES.fetch_values(char)
+    POINT_VALUES.fetch_values(char)
   end
   total_points = points.flatten.sum
   total_points += 8 if word_array.length >= 7
@@ -113,34 +112,34 @@ def highest_score_from(words)
     all_words << word_data
   end
 
-  tie_score = []
+  tie_scores = []
   all_words.each do |hash|
     current_score = hash[:score]
     if current_score > max_score
       max_score = current_score
-      tie_score = [hash]
-      tie_score << hash
+      tie_scores = [hash]
     elsif current_score == max_score
-      tie_score << hash
+      tie_scores << hash
     end
   end
 
-  smallest = 11
+  shortest_length = 11
   shortest_words = []
 
-  # if only one element in tie_score, that's the winner
-  return tie_score[0] if tie_score.length == 1
+  # if only one element in tie_scores, that's the winner
+  return tie_scores[0] if tie_scores.length == 1
 
-  tie_score.each do |tie|
+  tie_scores.each do |tie|
     # if 10 chars long, that's the winner
     if tie[:word].length == 10
       return tie
-    # if smaller word is found, update smallest and create a new shortest_words array to collect any possible ties for shortest length
-    elsif tie[:word].length < smallest
-      smallest = tie[:word].length
+    # if smaller word is found, update shortest_length and create a new shortest_words array to collect
+    # any possible ties for shortest length
+    elsif tie[:word].length < shortest_length
+      shortest_length = tie[:word].length
       shortest_words = [tie]
-      # if more than one word ties for smallest, put it into existing shortest_words array
-    elsif tie[:word].length == smallest
+      # if more than one word ties for shortest_length, put it into existing shortest_words array
+    elsif tie[:word].length == shortest_length
       shortest_words << tie
     end
   end
@@ -151,10 +150,8 @@ end
 
 # Wave 5
 def is_in_english_dict?(input)
-  found_word = []
-dictionary = CSV.read("../assets/dictionary-english.csv", headers: true).each do |record|
-  if record["Word"] == input.downcase
-      puts "Your word was found."
+  dictionary = CSV.read(DICTIONARY, headers: true).each do |word_entry|
+    if word_entry["Word"] == input.downcase
       return true
     end
   end
