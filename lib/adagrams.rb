acad200c629e4798b14f1e3eb79b2aa198aed7eb
RebeccaRoach <rebeccaroach12@gmail.com>
2020-02-13 16:10:54 -0800

    Added wave 5 method and removed comments.

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,3 +1,5 @@
+require "csv"
+require "awesome_print"
 
 $LETTERS = {
   "A" => 9,
@@ -29,7 +31,6 @@ $LETTERS = {
 }
 
 # Wave 1
-# method that returns shuffled hand of 10 tiles according to letter distribution hash
 def draw_letters
   pool = []
   $LETTERS.map do |letter, tiles|
@@ -42,7 +43,6 @@ def draw_letters
 end
 
 # Wave 2
-# method that returns true if every letter in the input word is available in correct quantities
 def uses_available_letters?(input, letters_in_hand)
   input_array = Array.new(input.chars)
   current_hand = Array.new(letters_in_hand)
@@ -60,13 +60,7 @@ def uses_available_letters?(input, letters_in_hand)
   return true
 end
 
-my_letters = draw_letters
-puts "Here are my letters: #{my_letters}"
-puts uses_available_letters?("EAIIIIIAAARR", my_letters)
-
 # Wave 3
-# Method that calculates word score according to Adagrams rules
-
 $LETTER_VALUES = {
   "A" => 1,	
   "B" => 3,	
@@ -103,38 +97,15 @@ def score_word(word)
     $LETTER_VALUES.fetch_values(char)
   end
   total_points = points.flatten.sum
-  if word_array.length >= 7
-    total_points += 8
-  end
+  total_points += 8 if word_array.length >= 7
   return total_points
 end
 
 # Wave 4
-# We want a way to find the highest scoring word.
-# This method looks at the array of words and calculates which of these words has the highest score, applies any tie-breaking logic, and returns the winning word in a special data structure.
-
-# Has one parameter: words, which is an array of strings
-# Returns a single hash that represents the data of a winning word and its score. The hash should have the following keys:
-# :word, whose value is a string of a word
-# :score, whose value is the score of that word
-# In the case of tie in scores, use these tie-breaking rules:
-# prefer the word with the fewest letters...
-# ...unless one word has 10 letters. If the top score is tied between multiple words and one is 10 letters long, choose the one with 10 letters over the one with fewer tiles
-# If the there are multiple words that are the same score and the same length, pick the first one in the supplied list
-
-# So, in a nut shell:   
-# first look at scores
-# if two scores are ==, then look at length
-# if 10 letters long, choose this one as winner
-# if same score and same length, pick first ordered as winner
-
-words = %w[cat dog]
-
 def highest_score_from(words)
   all_words = []
   max_score = 0
 
-  # Builds word info hashes and adds into all_words
   words.each do |word|
     word_data = {}
     word_data[:word] = word
@@ -142,29 +113,23 @@ def highest_score_from(words)
     all_words << word_data
   end
 
-
-# Eliminates non-ties for highest score
   tie_score = []
-
   all_words.each do |hash|
     current_score = hash[:score]
     if current_score > max_score
       max_score = current_score
       tie_score = [hash]
+      tie_score << hash
     elsif current_score == max_score
       tie_score << hash
     end
   end
 
-  # Eliminates non-ties for shortest length
-  # picking 11 to initialize smallest so that we're guaranteed to see smallest be replaced at least once
   smallest = 11
   shortest_words = []
 
   # if only one element in tie_score, that's the winner
-  if tie_score.length == 1
-    return tie_score[0]
-  end
+  return tie_score[0] if tie_score.length == 1
 
   tie_score.each do |tie|
     # if 10 chars long, that's the winner
@@ -180,9 +145,18 @@ def highest_score_from(words)
     end
   end
   
-  # if winner wasn't already found, the winner is simply the first item of shortest_words
+  # if winner wasn't already found, the winner is the first item of shortest_words
   return shortest_words[0]
-
 end
 
-puts highest_score_from(words)
+# Wave 5
+def is_in_english_dict?(input)
+  found_word = []
+dictionary = CSV.read("../assets/dictionary-english.csv", headers: true).each do |record|
+  if record["Word"] == input.downcase
+      puts "Your word was found."
+      return true
+    end
+  end
+  return false
+end
